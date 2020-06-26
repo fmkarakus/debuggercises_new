d756a744ddb712c7df84578a0597641de35e729e
fatih <fatihkarakus@hotmail.com>
2020-06-26 16:46:54 +0200

    Completed exercises from 11 to 18

--- exercises/12-functions-301/1-fill-in-the-blanks/1-write-tests.js
+++ exercises/12-functions-301/1-fill-in-the-blanks/1-write-tests.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // fill in the blanks to write 9 passing test cases for this function
 // be curious!  what happens if you use strange numbers like
@@ -11,47 +11,53 @@
  * @returns {number} the sum of x and y
  */
 const add = (x, y) => {
-  if (typeof x !== 'number') { throw new TypeError('x'); }
-  if (typeof y !== 'number') { throw new TypeError('y'); }
+  if (typeof x !== "number") {
+    throw new TypeError("x");
+  }
+  if (typeof y !== "number") {
+    throw new TypeError("y");
+  }
 
   const result = x + y;
 
-  if (typeof result !== 'number') { throw new TypeError('result'); }
+  if (typeof result !== "number") {
+    throw new TypeError("result");
+  }
   return result;
 };
 
-const _1_expect = _;
-const _1_actual = add(_);
-console.assert(_1_actual === _1_expect, 'Test 1');
+const _1_expect = 5;
+const _1_actual = add(2, 3);
+console.assert(_1_actual === _1_expect, "Test 1");
 
-const _2_expect = _;
-const _2_actual = add(_);
-console.assert(_2_actual === _2_expect, 'Test 2');
+const _2_expect = Infinity;
+const _2_actual = add(Infinity, 5);
+console.assert(_2_actual === _2_expect, "Test 2");
 
-const _3_expect = _;
-const _3_actual = add(_);
-console.assert(_3_actual === _3_expect, 'Test 3');
+const _3_expect = 2;
+const _3_actual = add(0.5, 1.5);
+console.assert(_3_actual === _3_expect, "Test 3");
 
-const _4_expect = _;
-const _4_actual = add(_);
-console.assert(_4_actual === _4_expect, 'Test 4');
+const _4_expect = 0;
+const _4_actual = add(-0.0, 0);
+console.assert(_4_actual === _4_expect, "Test 4");
 
-const _5_expect = _;
-const _5_actual = add(_);
-console.assert(_5_actual === _5_expect, 'Test 5');
+const _5_expect = 0.09999999999999998;
+const _5_actual = add(-0.2, 0.3);
+console.assert(_5_actual === _5_expect, "Test 5");
 
-const _6_expect = _;
-const _6_actual = add(_);
-console.assert(_6_actual === _6_expect, 'Test 6');
+const _6_expect = 1;
+const _6_actual = add(2 / 7, 5 / 7);
+console.assert(_6_actual === _6_expect, "Test 6");
 
-const _7_expect = _;
-const _7_actual = add(_);
-console.assert(_7_actual === _7_expect, 'Test 7');
+const _7_expect = 0.5;
+const _7_actual = add(2 / 7, 1.5 / 7);
+console.assert(_7_actual === _7_expect, "Test 7");
 
-const _8_expect = _;
-const _8_actual = add(_);
-console.assert(_8_actual === _8_expect, 'Test 8');
+const _8_expect = -Infinity;
+const _8_actual = add(-Infinity, 0);
+console.assert(_8_actual === _8_expect, "Test 8");
 
-const _9_expect = _;
-const _9_actual = add(_);
-console.assert(_9_actual === _9_expect, 'Test 9');
+const _9_expect = -4;
+const _9_actual = add(1, -5);
+console.assert(_9_actual === _9_expect, "Test 9");
--- exercises/12-functions-301/2-fix-the-bugs/2.js
+++ exercises/12-functions-301/2-fix-the-bugs/2.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // fix the mistakes in this conditional
 //  all of the bugs are in the conditional statement
@@ -11,43 +11,48 @@
  * @returns {boolean}
  */
 const areNotSameLength = (str1, str2) => {
-  if (typeof str1 !== 'string') { throw new TypeError('str1'); }
-  if (typeof str2 !== 'string') { throw new TypeError('str2'); }
-
-  let result;
-  if (str1.Length = str2.Length) {
-    result = result + true;
-  } else {
-    result = result + false;
+  if (typeof str1 !== "string") {
+    throw new TypeError("str1");
+  }
+  if (typeof str2 !== "string") {
+    throw new TypeError("str2");
   }
 
-  if (typeof result !== 'boolean') { throw new TypeError('result'); }
+  let result;
+  if (str1.length === str2.length) {
+    result = false;
+  } else {
+    result = true;
+  }
+
+  if (typeof result !== "boolean") {
+    throw new TypeError("result");
+  }
   return result;
 };
-
 
 // all of the tests are correct, there are not bugs below here!
 
 const _1_expect = true;
-const _1_actual = areNotSameLength('carboat', 'car boat');
-console.assert(_1_actual === _1_expect, 'Test 1');
+const _1_actual = areNotSameLength("carboat", "car boat");
+console.assert(_1_actual === _1_expect, "Test 1");
 
 const _2_expect = true;
-const _2_actual = areNotSameLength('water', 'waterfall');
-console.assert(_2_actual === _2_expect, 'Test 2');
+const _2_actual = areNotSameLength("water", "waterfall");
+console.assert(_2_actual === _2_expect, "Test 2");
 
 const _3_expect = true;
-const _3_actual = areNotSameLength('water fall', 'waterfall');
-console.assert(_3_actual === _3_expect, 'Test 3');
+const _3_actual = areNotSameLength("water fall", "waterfall");
+console.assert(_3_actual === _3_expect, "Test 3");
 
 const _4_expect = true;
-const _4_actual = areNotSameLength('birch', 'oak');
-console.assert(_4_actual === _4_expect, 'Test 4');
+const _4_actual = areNotSameLength("birch", "oak");
+console.assert(_4_actual === _4_expect, "Test 4");
 
 const _5_expect = false;
-const _5_actual = areNotSameLength('aspen', 'birch');
-console.assert(_5_actual === _5_expect, 'Test 5');
+const _5_actual = areNotSameLength("aspen", "birch");
+console.assert(_5_actual === _5_expect, "Test 5");
 
 const _6_expect = false;
-const _6_actual = areNotSameLength('hi!', 'bye');
-console.assert(_6_actual === _6_expect, 'Test 6');
+const _6_actual = areNotSameLength("hi!", "bye");
+console.assert(_6_actual === _6_expect, "Test 6");
